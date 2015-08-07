20929c3e0390dacd7f3a0dd7f14e7ef9a8e4bf8f
kmmmmmj <m950806@naver.com>
2015-08-07 17:52:51 +0900

    스피터 추가

--- app/src/main/java/com/example/ppangg/mapzipproject/home_Fragment.java
+++ app/src/main/java/com/example/ppangg/mapzipproject/home_Fragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class home_Fragment extends Fragment {
@@ -18,6 +20,8 @@ public class home_Fragment extends Fragment {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         user = UserData.getInstance();
+
+
     }
 
     @Override
@@ -31,6 +35,14 @@ public class home_Fragment extends Fragment {
         topstate.append("(");
         topstate.append(user.getUserID());
         topstate.append(")");
+
+        //스피너
+        Spinner spinner = (Spinner)v.findViewById(R.id.spinner);
+        //어댑터 생성
+        //이 예제 같은 경우 string,xml에 리스트를 추가해 놓고 그 리스트를 불러온다.
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_number, android.R.layout.simple_spinner_item);
+        //스피너와 어댑터 연결
+        spinner.setAdapter(adapter);
 
         return v;
     }
