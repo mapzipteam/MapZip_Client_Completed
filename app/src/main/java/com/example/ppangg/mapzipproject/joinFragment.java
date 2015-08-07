da6e807bc88881211ed7041410018fc1f64f4e1e
kmmmmmj <m950806@naver.com>
2015-08-07 16:25:28 +0900

    네비+현재

--- app/src/main/java/com/example/ppangg/mapzipproject/joinFragment.java
+++ app/src/main/java/com/example/ppangg/mapzipproject/joinFragment.java
@@ -12,14 +12,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.example.ppangg.mapzipproject.network.MyVolley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,6 +45,8 @@ public class joinFragment extends Fragment {
 
     private Button JoinBtn;
 
+    private View layout;
+    private TextView text;
 
     public static joinFragment create(int pageNumber) {
         joinFragment fragment = new joinFragment();
@@ -53,6 +60,12 @@ public class joinFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPageNumber = getArguments().getInt("page");
+
+        cont=getActivity();
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        layout = inflater.inflate(R.layout.my_custom_toast, (ViewGroup) getActivity().findViewById(R.id.custom_toast_layout));
+        text = (TextView)layout.findViewById(R.id.textToShow);
     }
 
     @Override
@@ -75,13 +88,13 @@ public class joinFragment extends Fragment {
 
         return rootView;
     }
-/*
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    /*
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
 
-    }
-*/
+        }
+    */
     public void DoJoin(View v) {
         RequestQueue queue = MyVolley.getInstance(cont).getRequestQueue();
         String url = SystemMain.SERVER_JOIN_URL;
@@ -89,10 +102,22 @@ public class joinFragment extends Fragment {
         final String userpw = inputPW.getText().toString();
         final String username = inputName.getText().toString();
         if (userid != null && !userid.equals("") && userpw != null && !userpw.equals("") && username != null && !username.equals("")) {
-            StringRequest myReq = new StringRequest(Request.Method.POST,
-                    url,
-                    NetSuccessListener(),
-                    NetErrorListener()) {
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("userid", userid);
+                obj.put("userpw", userpw);
+                obj.put("username",username);
+                Log.v("제이손 보내기",obj.toString());
+            } catch (JSONException e) {
+                Log.v("제이손", "에러");
+            }
+
+            JsonObjectRequest myReq = new JsonObjectRequest(Request.Method.POST,
+                    SystemMain.SERVER_JOIN_URL,
+                    obj,
+                    createMyReqSuccessListener(),
+                    createMyReqErrorListener()) {
+                /*
                 @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
                     Map<String, String> params = new HashMap<String, String>();
@@ -100,37 +125,61 @@ public class joinFragment extends Fragment {
                     params.put("userpw", userpw);
                     params.put("username", username);
                     return params;
-                }
+                }*/
             };
             queue.add(myReq);
         }
     }
 
-    private Response.Listener<String> NetSuccessListener() {
-        return new Response.Listener<String>() {
+    private Response.Listener<JSONObject> createMyReqSuccessListener() {
+        return new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(String response) {
-                Log.v("내용", response);
-                Log.v("길이", String.valueOf(response.length()));
+            public void onResponse(JSONObject response) {
 
-                if(response.substring(3).equals("1")) {
-                    state.setText("회원가입에 성공하였습니다.");
-                    Log.v("회원가입", "성공");
-                }
-                else {
-                    state.setText("이미 존재하는 계정입니다.");
-                    Log.v("회원가입", "실패");
+                try {
+                    Log.v("제이손", response.toString());
+
+                    if (response.get("join").toString().equals("1")) {
+                        // toast
+                        text.setText("회원가입에 성공하였습니다.");
+                        Toast toast = new Toast(cont);
+                        toast.setDuration(Toast.LENGTH_LONG);
+                        toast.setView(layout);
+                        toast.show();
+
+                        //state.setText("회원가입에 성공하였습니다.");
+                        Log.v("회원가입", "성공");
+                    } else {
+                        // toast
+                        text.setText("이미 존재하는 계정정보입니다.");
+                        Toast toast = new Toast(cont);
+                        toast.setDuration(Toast.LENGTH_LONG);
+                        toast.setView(layout);
+                        toast.show();
+
+                        //state.setText("이미 존재하는 계정입니다.");
+                        Log.v("회원가입", "실패");
+                    }
+                }catch (JSONException e){
+                    Log.v("에러", "제이손");
                 }
             }
         };
     }
 
-    private Response.ErrorListener NetErrorListener() {
+    private Response.ErrorListener createMyReqErrorListener() {
         return new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("회원가입",error.getMessage());
-                state.setText("인터넷 연결이 필요합니다.");
+                // toast
+                text.setText("인터넷 연결이 필요합니다.");
+                Toast toast = new Toast(cont);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+
+                Log.e("회원가입", error.getMessage());
+                //state.setText("인터넷 연결이 필요합니다.");
             }
         };
     }
