418e999e8d818d676f414ff0b2ae5e6bc8c74472
yucaroll <ghldjfldpsej@naver.com>
2015-08-10 16:40:52 +0900

    홈 하는 중

--- app/src/main/java/com/example/ppangg/mapzipproject/SystemMain.java
+++ app/src/main/java/com/example/ppangg/mapzipproject/SystemMain.java
@@ -1,5 +1,7 @@
 package com.example.ppangg.mapzipproject;
 
+import java.util.ArrayList;
+
 /**
  * Created by ljs93kr on 2015-07-27.
  * �� ���� ������ ���������� ����� ������� �Լ����� static���� ������ ��
@@ -12,6 +14,9 @@ public class SystemMain {
     public static final String SERVER_ROOT_URL = "http://ljs93kr.cafe24.com/mapzip"; // ������ ��Ʈurl
     public static final String SERVER_JOIN_URL = SERVER_ROOT_URL+"/login/joincheck.php"; // ������ join url
     public static final String SERVER_LOGIN_URL = SERVER_ROOT_URL+"/login/logincheck.php";
+
+    // 지도 갯수 디폴트값
+    public static final int DEFAULT_MAP_COUNT = 2;
 
 
 }
--- app/src/main/java/com/example/ppangg/mapzipproject/home_Fragment.java
+++ app/src/main/java/com/example/ppangg/mapzipproject/home_Fragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class home_Fragment extends Fragment {
     private TextView topstate ;
     private UserData user;
+    private ArrayList<String> sppinerList;
+    private View imageview;
 
 	public home_Fragment(){}
 
@@ -21,6 +26,9 @@ public class home_Fragment extends Fragment {
         super.onCreate(savedInstanceState);
         user = UserData.getInstance();
 
+        sppinerList = new ArrayList<String>();
+        sppinerList.add("첫번째");
+        sppinerList.add("second");
 
     }
 
@@ -28,7 +36,8 @@ public class home_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View v = inflater.inflate(R.layout.homelayout, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        imageview = (View)v.findViewById(R.id.mapimage);
 
         topstate = (TextView)v.findViewById(R.id.topstate);
         topstate.setText(user.getUserName());
@@ -37,12 +46,29 @@ public class home_Fragment extends Fragment {
         topstate.append(")");
 
         //스피너
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_dropdown_item,sppinerList);
         Spinner spinner = (Spinner)v.findViewById(R.id.spinner);
-        //어댑터 생성
-        //이 예제 같은 경우 string,xml에 리스트를 추가해 놓고 그 리스트를 불러온다.
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_number, android.R.layout.simple_spinner_item);
-        //스피너와 어댑터 연결
+        //ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_number, android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0)
+                {
+                    imageview.setBackgroundResource(R.drawable.seoul);
+                }
+                else
+                {
+                    imageview.setBackgroundResource(R.drawable.p1);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         return v;
     }
