dd86bbc65bcef76e1ead2af57c18d8e143f8a3d5
yucaroll <ghldjfldpsej@naver.com>
2015-08-23 19:18:25 +0900

    리뷰화면 작성중

---
+++ app/src/main/java/com/example/ppangg/mapzipproject/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.ppangg.mapzipproject;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by ppangg on 2015-08-23.
+ */
+
+public class ImageAdapter extends PagerAdapter {
+    Context context;
+
+    private int[] GalImages = new int[] {
+            R.drawable.p9,
+            R.drawable.p10,
+            R.drawable.p11
+    };
+
+    ImageAdapter(Context context){
+        this.context=context;
+    }
+    @Override
+    public int getCount() {
+        return GalImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        ImageView imageView = new ImageView(context);
+//        int padding = context.getResources().getDimensionPixelSize(R.dimen.padding_medium);
+
+//        imageView.setPadding(padding, padding, padding, padding);
+        //imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setImageResource(GalImages[position]);
+
+        ((ViewPager) container).addView(imageView, 0);
+
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
--- app/src/main/java/com/example/ppangg/mapzipproject/review_register.java
+++ app/src/main/java/com/example/ppangg/mapzipproject/review_register.java
@@ -2,7 +2,15 @@ package com.example.ppangg.mapzipproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Created by ppangg on 2015-08-22.
@@ -13,6 +21,35 @@ public class review_register extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_regi);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_review_regi);
+        ImageAdapter imageadapter = new ImageAdapter(this);
+        viewPager.setAdapter(imageadapter);
+        ImageView emotion = (ImageView) findViewById(R.id.emotion_review_regi);
+        emotion.setImageResource(R.drawable.sample_emotion);
+
+        final EditText directEdit = (EditText) findViewById(R.id.editeval_review_regi);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_review_regi);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.spinner_review_regi));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 3) // 직접입력
+                    directEdit.setVisibility(View.VISIBLE);
+                else
+                    directEdit.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
