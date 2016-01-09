4d35dd5f375559004c1142c93a82d25a8ac985fa
이준수 <ljs93kr@gmail.com>
2016-01-09 09:45:11 +0900

    시작애니메이션 부분 강화했습니다. 이미지만 갈아끼우면 됩니다.

--- app/src/main/java/com/mapzip/ppang/mapzipproject/main/SplashActivity.java
+++ app/src/main/java/com/mapzip/ppang/mapzipproject/main/SplashActivity.java
@@ -4,27 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Handler;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.mapzip.ppang.mapzipproject.R;
 
 public class SplashActivity extends Activity {
 
-    private ImageView StartImage;
-    private Animation start_ani;
+    private ImageView StartImage_ma;
+    private ImageView StartImage_flag1;
+    private ImageView StartImage_zi;
+    private ImageView StartImage_flag2;
+    private Animation start_text_ani;
+    private Animation start_flag_ani_1;
+    private Animation start_flag_ani_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getActionBar().hide();
+
         setContentView(R.layout.activity_splash);
-        StartImage = (ImageView)findViewById(R.id.start_image);
-        start_ani = new AlphaAnimation(0.0f,1.0f);
-        start_ani.setDuration(2000);
-        StartImage.startAnimation(start_ani);
+        StartImage_ma = (ImageView)findViewById(R.id.start_ma_image);
+        StartImage_flag1 = (ImageView)findViewById(R.id.start_flag1_image);
+        StartImage_zi = (ImageView)findViewById(R.id.start_zi_image);
+        StartImage_flag2 = (ImageView)findViewById(R.id.start_flag2_image);
+        start_text_ani = AnimationUtils.loadAnimation(this,R.anim.start_alpha);
+        start_flag_ani_1 = AnimationUtils.loadAnimation(this,R.anim.start_up2down_1);
+        start_flag_ani_2 = AnimationUtils.loadAnimation(this,R.anim.start_up2down_2);
+        StartImage_ma.startAnimation(start_text_ani);
+        StartImage_flag1.startAnimation(start_flag_ani_1);
+        StartImage_zi.startAnimation(start_text_ani);
+        StartImage_flag2.startAnimation(start_flag_ani_2);
         Handler hd = new Handler();
-        hd.postDelayed(new splashhandler(),2000);
+        hd.postDelayed(new splashhandler(),2500);
     }
 
     private class splashhandler implements Runnable{
